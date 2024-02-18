20455f377e965415ae73b5fc8b3b83950966c8fa
joaofbmaia <22353787+joaofbmaia@users.noreply.github.com>
2024-02-18 03:40:25 +0000

    Preliminary Working version

--- tests/bubbletea.c
+++ tests/bubbletea.c
@@ -3,6 +3,18 @@
 #include "bubbletea-lns.h"
 
 int main(void) {
+    // Test Data
+    volatile unsigned char *test_data = (unsigned char *) 0x81000000;
+    volatile unsigned char *test_result = (unsigned char *) 0x82000000;
+
+    for (int i = 0; i < 64; i++) {
+        test_data[i] = i;
+    }
+
+    for (int i = 0; i < 64; i++) {
+        test_result[i] = 0;
+    }
+
     //printf("Addr: %x\n", (uint64_t) &test_bitstream);
     // Write the bitstream base address
     reg_write64(BUBBLETEA_BITSTREAM_BASE_ADDR, (uint64_t) &load_no_store);
@@ -22,5 +34,14 @@ int main(void) {
     while (reg_read8(BUBBLETEA_DONE) == 0) ;
     // The accelerator has finished running
 
+    // Check the result
+    for (int i = 0; i < 64; i++) {
+        if (test_result[i] != i) {
+            printf("Error: test_result[%d] = %d\n", i, test_result[i]);
+            return 1;
+        }
+    }
+
+    printf("Success\n");
     return 0;
 }
